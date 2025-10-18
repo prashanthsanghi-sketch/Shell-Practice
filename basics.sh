b554c00249f5e73a1dd91505309796508403b86a
Prashanth Sanghi <prashanth.sanghi@gmail.com>
2025-10-18 16:45:37 +0530

    Passing ARGUMENTs through command line

--- basics.sh
+++ basics.sh
@@ -1,6 +1,9 @@
 echo "hello world"
 
 NAME="Prashanth"
+COURSE=$1
+LEARNER=$2
 
 echo "$NAME is a good boy and learning $Learning "
 echo "$learning shell"
+echo "Course is $COURSE and learner is $LEARNER"
